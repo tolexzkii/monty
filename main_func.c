8397d297bbfab9a70211ad874c32deaaf016da20
Ade-ojo Tolulope <tolucreatart@gmail.com>
2022-12-15 14:39:45 -0800

    monty fix

--- main_func.c
+++ main_func.c
@@ -1,142 +1,90 @@
 #include "monty.h"
 
 /**
- * is_number - checks if string is a number
- * @str: string being passed
- * Return: 1 if string is a number, otherwise 0
+ *add_dnodeint_end - add a note at the end of the doubly link list
+ *@head: first position of linked list
+ *@n: data to store
+ *Return: a doubly linked list
  */
-int is_number(char *str)
+stack_t *add_dnodeint_end(stack_t **head, const int n)
 {
-	unsigned int i;
+	stack_t *temp, *aux;
 
-	if (str == NULL)
-		return (0);
-	i = 0;
-	while (str[i])
-	{
-		if (str[0] == '-')
-		{
-			i++;
-			continue;
-		}
-		if (!isdigit(str[i]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * parse_line - parse a line for an opcode and arguments
- * @line: line to be parsed
- * @stack: pointer to the head of the stack
- * @line_num: current line number
- * Return: opcode, or NULL if fail
- */
-char *parse_line(char *line, stack_t **stack, unsigned int line_num)
-{
-	char *opc, *push, *arg;
-	(void)stack;
-
-	opc = strtok(line, "\n ");
-	push = "push";
-	if (opc == NULL)
+	if (head == NULL)
 		return (NULL);
-
-	if (strcmp(opc, push) == 0)
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
 	{
-		arg = strtok(NULL, "\n ");
-		if (is_number(arg) == 1 && arg != NULL)
-		{
-			var_global.push_arg = atoi(arg);
-		}
-		else
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_num);
-			exit(EXIT_FAILURE);
-		}
-	}
-	return (opc);
-}
-
-/**
- * get_op_func - checks opcode and return correct function
- * @str: opcode
- * Return: function, or NULL if fail
- */
-instruct_func get_op_func(char *str)
-{
-	int i;
-
-	inst_t instruct[] = {
-		{"push", _push},
-		{"pall", _pall},
-		{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"add", _add},
-		{"nop", _nop},
-		{"sub", _sub},
-		{"mul", _mul},
-		{"div", _div},
-		{"mod", _mod},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"rotl", _rotl},
-		{"rotr", _rotr},
-		{NULL, NULL},
-	};
-
-	i = 0;
-	while (instruct[i].f != NULL && strcmp(instruct[i].opcode, str) != 0)
-	{
-		i++;
-	}
-
-	return (instruct[i].f);
-}
-
-/**
- * read_file - reads a bytecode file and run commands
- * @filename: name of file
- * @stack: pointer to the top of the stack
- */
-void read_file(char *filename, stack_t **stack)
-{
-	char *line;
-	size_t i = 0;
-	int line_count = 1;
-	instruct_func a;
-	int check;
-	int read;
-
-	var_global.file = fopen(filename, "r");
-
-	if (var_global.file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		dprintf(2, "Error: malloc failed\n");
+		free_vglo();
 		exit(EXIT_FAILURE);
 	}
-
-	while ((read = getline(&var_global.buffer, &i, var_global.file)) != -1)
+	temp->n = n;
+	/*Careful with the first time*/
+	if (*head == NULL)
 	{
-		line = parse_line(var_global.buffer, stack, line_count);
-		if (line == NULL || line[0] == '#')
-		{
-			line_count++;
-			continue;
-		}
-		a = get_op_func(line);
-		if (a == NULL)
-		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_count, line);
-			exit(EXIT_FAILURE);
-		}
-		a(stack, line_count);
-		line_count++;
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
 	}
-	free(var_global.buffer);
-	check = fclose(var_global.file);
-	if (check == -1)
-		exit(-1);
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	temp->next = aux->next;
+	temp->prev = aux;
+	aux->next = temp;
+	return (aux->next);
+}
+
+/**
+ *add_dnodeint - add a note at the begining of the doubly link list
+ *@head: first position of linked list
+ *@n: data to store
+ *Return: a doubly linked list
+ */
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Careful with the first time*/
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	(*head)->prev = temp;
+	temp->next = (*head);
+	temp->prev = NULL;
+	*head = temp;
+	return (*head);
+}
+
+/**
+ * free_dlistint - frees the doubly linked list
+ *
+ * @head: head of the list
+ * Return: no return
+ */
+void free_dlistint(stack_t *head)
+{
+	stack_t *tmp;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
 }
--- monty.h
+++ monty.h
@@ -1,19 +1,22 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef MONTY
+#define MONTY
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <ctype.h>
 
-extern int push_arg;
-
 /**
- * struct stack_s - doubly linked list of a stack
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
- * @prev: points to the previous element of the stack
- * @next: points to the next element of the stack
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -23,59 +26,78 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct inst_s - opcodes and its functions
- * @opcode: opcode
- * @f: function for opcode
+ * struct globals - global structure to use in the functions
+ * @lifo: is stack or queue
+ * @cont: current line
+ * @arg: second parameter inside the current line
+ * @head: doubly linked list
+ * @fd: file descriptor
+ * @buffer: input text
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
  */
-typedef struct inst_s
+typedef struct globals
 {
-	char *opcode;
-	void (*f)(stack_t ** stack, unsigned int line_num);
-} inst_t;
+	int lifo;
+	unsigned int cont;
+	char  *arg;
+	stack_t *head;
+	FILE *fd;
+	char *buffer;
+} global_t;
 
 /**
- * struct global_variable - opcode and function
- * @file: opcode
- * @push_arg: opcode function
- * @buffer: pointer
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
  */
-typedef struct global_variable
+typedef struct instruction_s
 {
-	FILE *file;
-	int push_arg;
-	char *buffer;
-} global_var;
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
-extern global_var var_global;
+extern global_t vglo;
 
-void read_file(char *filename, stack_t **stack);
-char *parse_line(char *line, stack_t **stack, unsigned int line_num);
-typedef void (*instruct_func)(stack_t **stack, unsigned int line_num);
-instruct_func get_op_func(char *str);
-int is_number(char *str);
+/* opcode_instructuions*/
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **doubly, unsigned int cline);
+void _pop(stack_t **doubly, unsigned int cline);
+void _swap(stack_t **doubly, unsigned int cline);
+void _queue(stack_t **doubly, unsigned int cline);
+void _stack(stack_t **doubly, unsigned int cline);
+void _add(stack_t **doubly, unsigned int cline);
+void _nop(stack_t **doubly, unsigned int cline);
+void _sub(stack_t **doubly, unsigned int cline);
+void _div(stack_t **doubly, unsigned int cline);
+void _mul(stack_t **doubly, unsigned int cline);
+void _mod(stack_t **doubly, unsigned int cline);
+void _pchar(stack_t **doubly, unsigned int cline);
+void _pstr(stack_t **doubly, unsigned int cline);
+void _rotl(stack_t **doubly, unsigned int cline);
+void _rotr(stack_t **doubly, unsigned int cline);
 
-void _push(stack_t **stack, unsigned int line_num);
-void _pall(stack_t **stack, unsigned int line_num);
-void _pint(stack_t **stack, unsigned int line_num);
-void _pop(stack_t **stack, unsigned int line_num);
-void _swap(stack_t **stack, unsigned int line_num);
-void _nop(stack_t **stack, unsigned int line_num);
-void _rotl(stack_t **stack, unsigned int line_num);
-void _rotr(stack_t **stack, unsigned int line_num);
+/*get function*/
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
 
+/*imported functions*/
+int _sch(char *s, char c);
+char *_strtoky(char *s, char *d);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int _strcmp(char *s1, char *s2);
 
-void _sub(stack_t **stack, unsigned int line_num);
-void _add(stack_t **stack, unsigned int line_num);
-void _div(stack_t **stack, unsigned int line_num);
-void _mul(stack_t **stack, unsigned int line_num);
-void _mod(stack_t **stack, unsigned int line_num);
+/* doubly linked list functions */
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+stack_t *add_dnodeint(stack_t **head, const int n);
+void free_dlistint(stack_t *head);
 
-void _pchar(stack_t **stack, unsigned int line_num);
-void _pstr(stack_t **stack, unsigned int line_num);
-
-void dobfree(stack_t **stack);
-void free_listint(stack_t *head);
-void dobf(stack_t **stack);
-int _isalpha(int i);
+/* main */
+void free_vglo(void);
 
 #endif
